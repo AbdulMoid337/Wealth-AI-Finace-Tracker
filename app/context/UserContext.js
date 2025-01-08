a72f28e39f717821602297bdc75b50170cb42d27
Abdul Moid <abdulmoid337@gmail.com>
2025-01-09 02:50:22 +0530

    last

--- app/context/UserContext.js
+++ app/context/UserContext.js
@@ -1,18 +1,24 @@
-"use client"
+"use client";
 import { createContext, useContext, useState, useEffect } from "react";
 
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [userInfo, setUserInfo] = useState(() => {
-    // Load user info from local storage if available
-    const savedUserInfo = localStorage.getItem("userInfo");
-    return savedUserInfo ? JSON.parse(savedUserInfo) : {};
+    // Check if running in the browser
+    if (typeof window !== "undefined") {
+      // Load user info from local storage if available
+      const savedUserInfo = localStorage.getItem("userInfo");
+      return savedUserInfo ? JSON.parse(savedUserInfo) : {};
+    }
+    return {}; // Return an empty object if not running in the browser
   });
 
   useEffect(() => {
-    // Save user info to local storage whenever it changes
-    localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    // Save user info to local storage whenever it changes (only client-side)
+    if (typeof window !== "undefined") {
+      localStorage.setItem("userInfo", JSON.stringify(userInfo));
+    }
   }, [userInfo]);
 
   return (
@@ -24,4 +30,4 @@ export const UserProvider = ({ children }) => {
 
 export const useUser = () => useContext(UserContext);
 
-export { UserContext }; 
+export { UserContext };
