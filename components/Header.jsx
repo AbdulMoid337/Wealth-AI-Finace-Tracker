cd352761d5f8af4f0d7b9e17a8752c31ac744063
Abdul Moid <abdulmoid337@gmail.com>
2025-04-05 04:01:36 +0530

    logo update

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,10 +14,10 @@ const Header = async () => {
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
         <Link href="/">
           <Image
-            src="/logo.png"
+            src="/welth.png"
             alt="Welth Logo"
-            width={200}
-            height={60}
+            width={90}
+            height={90}
             className="h-12 w-auto object-contain"
           />
         </Link>
